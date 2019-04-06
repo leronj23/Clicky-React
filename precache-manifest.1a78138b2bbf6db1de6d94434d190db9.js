f3f028c7e6fe80242f130bd7bdb6830180b16c13
LeRonj23 <leronj23@gmail.com>
2019-04-06 15:08:20 -0400

    Updates

---
+++ precache-manifest.1a78138b2bbf6db1de6d94434d190db9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4a24ad327cd80267916",
+    "url": "/Clicky-React/static/js/runtime~main.6b842b1c.js"
+  },
+  {
+    "revision": "22a0ad79cc62bc597f03",
+    "url": "/Clicky-React/static/js/main.19c2a0bb.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-React/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "22a0ad79cc62bc597f03",
+    "url": "/Clicky-React/static/css/main.248d9de5.chunk.css"
+  },
+  {
+    "revision": "5b53a501439b77b80e3c27dfb39f286c",
+    "url": "/Clicky-React/index.html"
+  }
+];
